4a7ce7adeb2af9b65f7310612394f50c8b1d6ca2
Restyled.io <commits@restyled.io>
2020-09-10 10:27:18 +0000

    Restyled by autopep8

--- userbot/plugins/eye.py
+++ userbot/plugins/eye.py
@@ -7,9 +7,7 @@ import asyncio
 from userbot.utils import admin_cmd
 
 
-
 @borg.on(admin_cmd(pattern="eye"))
-
 async def _(event):
 
     if event.fwd_from:
@@ -22,23 +20,23 @@ async def _(event):
 
     #input_str = event.pattern_match.group(1)
 
-    #if input_str == "eye":
+    # if input_str == "eye":
 
     await event.edit("👁👁")
 
     animation_chars = [
 
-            "👁👁\n  👄  =====> Abey Ja Na sixer",
-            "👁👁\n  👅  =====> Abey Ja Na Madarchod",    
-            "👁👁\n  💋  =====> Abey Ja Na Randi",
-            "👁👁\n  👄  =====> Abey Ja Na Betichod",
-            "👁👁\n  👅  =====> Abey Ja Na Behenchod",    
-            "👁👁\n  💋  =====> Abey Ja Na Na Mard",
-            "👁👁\n  👄  =====> Abey Ja Na Randi",
-            "👁👁\n  👅  =====> Abey Ja Na Bhosdk",    
-            "👁👁\n  💋  =====> Abey Ja Na Chutiye",
-            "👁👁\n  👄  =====> Hi All, How Are You Guys..."
-        ]
+        "👁👁\n  👄  =====> Abey Ja Na sixer",
+        "👁👁\n  👅  =====> Abey Ja Na Madarchod",
+        "👁👁\n  💋  =====> Abey Ja Na Randi",
+        "👁👁\n  👄  =====> Abey Ja Na Betichod",
+        "👁👁\n  👅  =====> Abey Ja Na Behenchod",
+        "👁👁\n  💋  =====> Abey Ja Na Na Mard",
+        "👁👁\n  👄  =====> Abey Ja Na Randi",
+        "👁👁\n  👅  =====> Abey Ja Na Bhosdk",
+        "👁👁\n  💋  =====> Abey Ja Na Chutiye",
+        "👁👁\n  👄  =====> Hi All, How Are You Guys..."
+    ]
 
     for i in animation_ttl:
 
